f516bffc366f00063f52723bae508590819f182d
Dmitry Shlagoff <wessmoke@gmail.com>
2016-12-24 19:30:25 +0700

    Using repl (buggy).

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+extern crate users;
+
 mod token;
 mod lexer;
+mod repl;
 
-fn main() {}
+use std::io::stdin;
+use std::io::stdout;
+
+use users::get_user_by_uid;
+use users::get_current_uid;
+
+use repl::start;
+
+fn main() {
+    let user = get_user_by_uid(get_current_uid()).unwrap();
+    println!("Hello {}! This is the Monkey programming language!", user.name());
+    println!("Feel free to type in commands");
+
+    start(stdin(), stdout());
+}
