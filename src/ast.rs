eca5cd8889fecacb4925db1f09199d8fbf5c07fe
Dmitry Shlagoff <wessmoke@gmail.com>
2016-12-25 15:54:16 +0700

    More idiomatic Rust + tests.

--- src/ast.rs
+++ src/ast.rs
@@ -4,20 +4,38 @@ pub trait Node<'a> {
     fn token_literal(&'a self) -> &'a str;
 }
 
-pub trait Statement<'a>: Node<'a> {
-    fn statement_node(&self);
+pub enum Statement<'a> {
+    Let {
+        token: Token,
+        name: &'a Identifier,
+        value: Expression,
+    },
+    Empty,
 }
 
-pub trait Expression<'a>: Node<'a> {
-    fn expression_nod(&self);
+impl<'a> Node<'a> for Statement<'a> {
+    fn token_literal(&self) -> &str {
+        match *self {
+            Statement::Let { ref token, .. } => &token.literal,
+            ref Empty => "",
+        }
+    }
 }
 
-struct Program<'a> {
-    statements: Vec<&'a Statement<'a>>,
+pub enum Expression {}
+
+pub struct Program<'a> {
+    pub statements: Vec<Statement<'a>>,
+}
+
+impl<'a> Program<'a> {
+    pub fn new() -> Self {
+        Program { statements: Vec::new() }
+    }
 }
 
 impl<'a> Node<'a> for Program<'a> {
-    fn token_literal(&self) -> &'a str {
+    fn token_literal(&self) -> &str {
         if self.statements.len() > 0 {
             self.statements[0].token_literal()
         } else {
@@ -26,31 +44,10 @@ impl<'a> Node<'a> for Program<'a> {
     }
 }
 
-struct LetStatement<'a, E>
-    where E: Expression<'a>
-{
+#[derive(Debug)]
+pub struct Identifier {
     token: Token,
-    name: &'a Identifier,
-    value: E,
-}
-
-impl<'a, E> Node<'a> for LetStatement<'a, E>
-    where E: Expression<'a>
-{
-    fn token_literal(&'a self) -> &'a str {
-        &self.token.literal
-    }
-}
-
-impl<'a, E> Statement<'a> for LetStatement<'a, E>
-    where E: Expression<'a>
-{
-    fn statement_node(&self) {}
-}
-
-struct Identifier {
-    token: Token,
-    value: String,
+    pub value: String,
 }
 
 impl<'a> Node<'a> for Identifier {
--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,8 @@
 use super::lexer::Lexer;
 use super::token::Token;
-use super::ast;
+use super::ast::Program;
+use super::ast::Node;
+use super::ast::Statement;
 
 
 struct Parser<'a> {
@@ -26,5 +28,50 @@ impl<'a> Parser<'a> {
     fn next_token(&mut self) {
         self.cur_token = self.peek_token.clone();
         self.peek_token = Some(self.lexer.next_token());
+    }
+
+    fn parse_program(&self) -> Program {
+        Program::new()
+    }
+}
+
+
+#[test]
+fn test_let_statements() {
+    let input = String::from("
+let x = 5;
+let y = 10;
+let foobar = 838383;
+    ");
+
+    let mut l = Lexer::new(input);
+    let p = Parser::new(&mut l);
+
+    let program = p.parse_program();
+    assert!(program.statements.len() == 3,
+            "program.statements does not contain 3 statements. Got {}",
+            program.statements.len());
+
+    let tests = vec!["x", "y", "foobar"];
+
+    for ((i, expected), stmt) in tests.iter().enumerate().zip(program.statements) {
+        assert!(stmt.token_literal() == "let",
+                "[{}] stmt.token_literal not 'let'. Got {}",
+                i,
+                stmt.token_literal());
+
+        if let Statement::Let { name, .. } = stmt {
+            assert!(name.value == *expected,
+                    "stmt.name.value is not {}. Got {}",
+                    expected,
+                    name.value);
+
+            assert!(name.token_literal() == *expected,
+                    "stmt.name is not {}. Got {:?}",
+                    expected,
+                    name);
+        } else {
+            panic!("stmt is not Statement::Let");
+        }
     }
 }
